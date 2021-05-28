40f27af0c0211355e2e1cd97f20017d361b9d5a0
Carlos Basulto <84686010+cbasultog-ipn@users.noreply.github.com>
2021-05-28 12:10:27 -0500

    Update TDC.py

--- TDC.py
+++ TDC.py
@@ -1,25 +1,6 @@
-#Declaramos las funciones Lambda
+#Biblioteca para convertir monedas a un tipo de cambio fijo.
+#Autor: Carlos Basulto / 05-2021
+
+#Declaramos las funciones Lambda para convertir pesos y dólares
 mxn = lambda x: x*20
 usd = lambda x: x/20
-
-#Convertimos pesos a dólares utilizando la función Lambda
-valorMXN = int(input("Dame un valor en pesos: "))
-valorUSD = int(usd(valorMXN))
-
-print("El total en dólares es: "+ str(valorUSD) + " dólares americanos")
-
-#Convertimos dólares a pesos utilizando la función Lambda
-valorUSD = int(input("Dame un valor en dólares: "))
-valorMXN = int(mxn(valorUSD))
-print("El total en pesos es: "+ str(valorMXN) + " pesos mexicanos")
-
-#¿Cómo sería sin Lambdas? Definamos una función para convertir a pesos a dólares.
-def convertirDolares (valorMXN):
-    dolares=valorMXN/20
-    return dolares
-
-valorMXN = int(input("Funcion - Dame un valor en pesos: "))
-valorUSD = int(convertirDolares(valorMXN))
-print("El total en dólares es: "+ str(valorUSD) + " dólares americanos")
-
-#Fin del programa
